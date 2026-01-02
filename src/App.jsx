ab316e22acbcba69ebfb7949ca384240c71cdf9e
Damiao Netto <143633691+DamiaoNetto@users.noreply.github.com>
2026-01-02 17:24:23 -0300

    Corrige imports para build no Linux/Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar"
 import  Hero from "./components/Hero"
-import { About } from "./components/About";
+import About  from "./components/About";
 import CardsTecnologias from "./components/CardsSkills";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
+import Contact from "./components/Contact";
+import  Footer from "./components/Footer";
 
 const App = () =>{
   return(
